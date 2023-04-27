aa00ee0a394808de72bf783c0c12856bb54b9924
JerryIshihara <hirotaka.ishihara@mail.utoronto.ca>
2023-04-26 20:08:18 -0700

    new sim

--- src/emittor.cc
+++ src/emittor.cc
@@ -7,9 +7,9 @@ Particle Emittor::emit() const
     double a = (((double)rand() / (double)RAND_MAX) - 0.5);
     double b = (((double)rand() / (double)RAND_MAX) - 0.5);
     double c = (((double)rand() / (double)RAND_MAX) - 0.5);
-    Vector3f init_pos = Vector3f(a * 10, b * 10, c * 10);
-    Vector3f rand_velocity = Vector3f(a / 1000, b * 10, c / 1000);
-    Vector3f init_forces = Vector3f(0, 0, 0);
+    Vector3f init_pos = Vector3f(a * 2, b * 10, c * 2);
+    Vector3f rand_velocity = Vector3f(a * 50, 0, b * 50);
+    Vector3f init_forces = Vector3f(a * 10, 0, 0);
     float init_size = 1;
     return Particle(init_pos, rand_velocity, init_forces, init_color, init_size);
 }
