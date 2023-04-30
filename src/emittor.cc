51f8b0c7f123bef49380a43c9dc6bfb1131c2237
Zhengjiang He <george.he1998@gmail.com>
2023-04-30 10:31:30 -0700

    Avoid new random generator at every step

--- src/emittor.cc
+++ src/emittor.cc
@@ -1,24 +1,16 @@
-#include <random>
 #include "emittor.hh"
 #include "smoke_param.hh"
 
 using namespace nanogui;
 
-Particle Emittor::emit(SmokeParameter *sp) const
+Particle Emittor::emit(SmokeParameter *sp)
 {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<double> angle_dis(0, 2 * M_PI);
-    std::uniform_real_distribution<double> radius_dis(0, 5);
-    std::uniform_real_distribution<double> height_dis(0, 5);
-    std::uniform_real_distribution<double> vel_dis(5, 10);
-
-    float angle = angle_dis(gen);
-    float radius = radius_dis(gen);
-    float height = height_dis(gen);
-    Vector3f init_pos(radius * cos(angle), height - 5, radius * sin(angle));
-    Vector3f rand_velocity(radius * cos(angle), vel_dis(gen) * 10, radius * sin(angle)); // Upward velocity
-    Vector3f init_forces(radius * cos(angle), 0, radius * sin(angle));
+    auto angle = angle_dis(gen);
+    auto radius = radius_dis(gen);
+    auto height = height_dis(gen);
+    Vector3f init_pos(radius * cos(angle), height - 5.0, radius * sin(angle));
+    Vector3f rand_velocity(radius * cos(angle), vel_dis(gen) * 10.0, radius * sin(angle)); // Upward velocity
+    Vector3f init_forces(radius * cos(angle), 0.0, radius * sin(angle));
 
     return Particle(init_pos, rand_velocity, init_forces, sp);
 }
--- src/emittor.hh
+++ src/emittor.hh
@@ -1,21 +1,37 @@
+#ifndef EMITTOR_H
+#define EMITTOR_H
+#include <random>
 #include <nanogui/nanogui.h>
 #include "particle.hh"
 
 class Emittor
 {
-
+    std::mt19937 gen;
+    std::uniform_real_distribution<double> angle_dis;
+    std::uniform_real_distribution<double> radius_dis;
+    std::uniform_real_distribution<double> height_dis;
+    std::uniform_real_distribution<double> vel_dis;
+    
 public:
-    Emittor(){};
+    
+    Emittor():gen(std::random_device{}()){};
 
-    Emittor(nanogui::Vector3f position, nanogui::Vector3f init_velocity, nanogui::Vector4f init_color)
+    Emittor(nanogui::Vector3f position, nanogui::Vector3f init_velocity, nanogui::Vector4f init_color) :
+        gen(std::random_device{}()),
+        angle_dis(0, 2 * M_PI),
+        radius_dis(0, 5),
+        height_dis(0, 5),
+        vel_dis(5, 10)
     {
         this->position = position;
         this->init_velocity = init_velocity;
         this->init_color = init_color;
     }
+    
     nanogui::Vector3f position;
     nanogui::Vector3f init_velocity;
     nanogui::Vector4f init_color;
 
-    Particle emit(SmokeParameter *sp) const;
+    Particle emit(SmokeParameter *sp);
 };
+#endif
